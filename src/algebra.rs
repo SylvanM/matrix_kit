52047ae9c31ee8f75ec909893d8830cdabc2feb6
Sylvan Martin <slm338@cornell.edu>
2024-04-25 11:20:49 -0400

    Added recovered files

---
+++ src/algebra.rs
@@ -0,0 +1,340 @@
+//
+// A collection of primitive algebraic structures
+//
+
+use std::fmt::Debug;
+// use rand::{rngs::StdRng, Rng, SeedableRng};
+use std::ops::{Add, Neg, AddAssign, Div, DivAssign, Mul, MulAssign, Sub, SubAssign};
+
+/**
+ * An algebraic ring
+ */
+pub trait Ring: Debug + PartialEq + Copy + Sized + Add<Self> + AddAssign<Self> + Neg + Sub<Self> + SubAssign<Self> + Mul<Self> + MulAssign<Self> + Mul<Output = Self> + Add<Output = Self> + Neg<Output = Self> + Sub<Output = Self> {
+
+	/**
+	 * The multiplicative identity of this ring
+	 */
+	fn one() -> Self;
+
+	/**
+	 * The additive identity of this ring
+	 */
+	 fn zero() -> Self;
+
+	/**
+	 * A ring element raised to a power
+	 */
+   	fn power(self, n: i64) -> Self;
+}
+
+/**
+ * A field, which is a ring where every nonzero element has a multiplicative inverse
+ */
+pub trait Field: Ring + Div + DivAssign + Div<Output = Self> {
+	fn inverse(self) -> Self;
+}
+
+// MARK: Implementations
+
+impl Ring for f64 {
+	fn one() -> Self {
+		1.0
+	}
+
+	fn zero() -> Self {
+		0.0
+	}
+
+	fn power(self, n: i64) -> Self {
+		self.powf(n as f64)
+	}
+}
+
+impl Field for f64 {
+	fn inverse(self) -> Self {
+		if self == 0.0 {
+			panic!("Cannot divide by zero")
+		} else {
+			1.0 / self
+		}
+	}
+}
+
+impl Ring for f32 {
+
+	fn one() -> Self {
+		1.0
+	}
+
+	fn zero() -> Self {
+		0.0
+	}
+
+	fn power(self, n: i64) -> Self {
+		self.powf(n as f32)
+	}
+}
+
+impl Field for f32 {
+	fn inverse(self) -> Self {
+		if self == 0.0 {
+			panic!("Cannot divide by zero")
+		} else {
+			1.0 / self
+		}
+	}
+}
+
+impl Ring for i8 {
+	fn one() -> Self {
+		1
+	}
+
+	fn zero() -> Self {
+		0
+	}
+
+	fn power(self, n: i64) -> Self {
+		if n < 0 {
+			panic!("Cannot invert ring element")
+		}
+		self.pow(n as u32)
+	}
+}
+
+impl Ring for i16 {
+	fn one() -> Self {
+		1
+	}
+
+	fn zero() -> Self {
+		0
+	}
+
+	fn power(self, n: i64) -> Self {
+		if n < 0 {
+			panic!("Cannot invert ring element")
+		}
+		self.pow(n as u32)
+	}
+}
+
+impl Ring for i32 {
+	fn one() -> Self {
+		1
+	}
+
+	fn zero() -> Self {
+		0
+	}
+
+	fn power(self, n: i64) -> Self {
+		if n < 0 {
+			panic!("Cannot invert ring element")
+		}
+		self.pow(n as u32)
+	}
+}
+
+impl Ring for i64 {
+	fn one() -> Self {
+		1
+	}
+
+	fn zero() -> Self {
+		0
+	}
+
+	fn power(self, n: i64) -> Self {
+		if n < 0 {
+			panic!("Cannot invert ring element")
+		}
+		self.pow(n as u32)
+	}
+}
+
+impl Ring for i128 {
+	fn one() -> Self {
+		1
+	}
+
+	fn zero() -> Self {
+		0
+	}
+
+	fn power(self, n: i64) -> Self {
+		if n < 0 {
+			panic!("Cannot invert ring element")
+		}
+		self.pow(n as u32)
+	}
+}
+
+/// The field of the integers modulo a prime Q
+#[derive(Clone, Copy, Default)]
+pub struct ZM<const Q: i64> {
+	pub val: i64
+}
+
+impl<const Q: i64> Debug for ZM<Q> {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		self.val.fmt(f)
+	}
+}
+
+impl<const Q: i64> PartialEq for ZM<Q> {
+	fn eq(&self, other: &Self) -> bool {
+		self.val == other.val
+	}
+
+	fn ne(&self, other: &Self) -> bool {
+		self.val != other.val
+	}
+}
+
+impl<const Q: i64> ZM<Q> {
+	pub fn convert<const P: i64>(other: ZM<P>) -> ZM<Q> {
+		other.val.into()
+	}
+
+	pub fn from_int(x: i64) -> ZM<Q> {
+		x.into()
+	}
+}
+
+impl<const Q: i64> From<i64> for ZM<Q> {
+	fn from(value: i64) -> Self {
+		ZM::<Q> { val: value.rem_euclid(Q) } 
+	}
+}
+
+impl<const Q: i64> From<u8> for ZM<Q> {
+	fn from(value: u8) -> Self {
+		ZM::<Q> { val: (value as i64).rem_euclid(Q) } 
+	}
+}
+
+impl<const Q: i64> From<i32> for ZM<Q> {
+	fn from(value: i32) -> Self {
+		ZM::<Q> { val: (value as i64).rem_euclid(Q) } 
+	}
+}
+
+impl<const Q: i64> Add<ZM<Q>> for ZM<Q> {
+	type Output = ZM<Q>;
+
+	fn add(self, rhs: ZM<Q>) -> Self::Output {
+		ZM::<Q> { val: (self.val + rhs.val) % Q }
+	}
+}
+
+impl<const Q: i64> AddAssign<ZM<Q>> for ZM<Q> {
+	fn add_assign(&mut self, rhs: ZM<Q>) {
+		*self = *self + rhs
+	}
+}
+
+impl<const Q: i64> Sub<ZM<Q>> for ZM<Q> {
+	type Output = ZM<Q>;
+
+	fn sub(self, rhs: ZM<Q>) -> Self::Output {
+		ZM::<Q> { val: (self.val - rhs.val + Q) % Q }
+	}
+}
+
+impl<const Q: i64> SubAssign<ZM<Q>> for ZM<Q> {
+	fn sub_assign(&mut self, rhs: ZM<Q>) {
+		*self = *self - rhs
+	}
+}
+
+impl<const Q: i64> Mul<ZM<Q>> for ZM<Q> {
+	type Output = ZM<Q>;
+
+	fn mul(self, rhs: ZM<Q>) -> ZM<Q> {
+		let product = self.val.rem_euclid(Q) * rhs.val.rem_euclid(Q);
+		ZM::<Q> { val: product % Q }
+	}
+}
+
+impl<const Q: i64> MulAssign<ZM<Q>> for ZM<Q> {
+	fn mul_assign(&mut self, rhs: ZM<Q>) {
+		*self = *self * rhs
+	}
+}
+
+impl<const Q: i64> Neg for ZM<Q> {
+	type Output = Self;
+
+	fn neg(self) -> Self::Output {
+		(Q - self.val).into()
+	}
+}
+
+impl<const Q: i64> Ring for ZM<Q> {
+	fn one() -> Self {
+		ZM::<Q> { val: 1 }
+	}
+
+	fn zero() -> Self {
+		ZM::<Q> { val: 0 }
+	}
+
+	fn power(self, n: i64) -> Self {
+		// TODO: Make this WAYY more efficient... Double and add, yeah?
+		let mut power = ZM::<Q>::one();
+
+		for _ in 1..=n {
+			power *= self
+		}
+
+		power
+	}
+}
+
+// impl<const Q: i64> ZM<Q> {
+// 	fn rnd() -> ZM<Q> {
+// 		ZM::<Q> { val: StdRng::from_entropy().gen::<i64>().rem_euclid(Q) }
+// 	}
+// }
+
+/// Returns (g, x, y) so that 
+/// - g = gcd(a, b)
+/// ax + by = gcd(a, b)
+fn ext_gcd(a: i64, b: i64) -> (i64, i64, i64) {
+
+	if a == 0 {
+		return (b, 0, 1)
+	}
+
+	let (g, x1, y1) = ext_gcd(b % a, a);
+
+	let x = y1 - (b/a) * x1;
+	let y = x1;
+
+	(g, x, y)
+}
+
+fn mod_inv(x: i64, m: i64) -> i64 {
+	match ext_gcd(x, m) { (_, i, _) => i }
+}
+
+impl<const Q: i64> Div<ZM<Q>> for ZM<Q> {
+	type Output = ZM<Q>;
+
+	fn div(self, rhs: ZM<Q>) -> Self::Output {
+		self * mod_inv(rhs.val, Q).into()
+	}
+}
+
+impl<const Q: i64> DivAssign<ZM<Q>> for ZM<Q> {
+	fn div_assign(&mut self, rhs: ZM<Q>) {
+		*self = *self / rhs
+	}
+}
+
+impl<const Q: i64> Field for ZM<Q> {
+	fn inverse(self) -> Self {
+		mod_inv(self.val, Q).into()
+	}
+}
---
+++ src/matrix.rs
@@ -0,0 +1,302 @@
+//
+// A matrix!
+//
+
+use std::{fmt::Debug, ops::{Add, AddAssign, Index, IndexMut, Mul, Neg, Sub, SubAssign}};
+
+use crate::algebra::*;
+
+#[macro_export]
+macro_rules! index {
+	($m: expr, $n: expr, $r: expr, $c: expr) => {
+		$c * $m + $r
+	};
+}
+
+/// Computes the dot product between the r-th row of the matrix x, and the vector y.
+/// 
+/// * `M` - 
+fn vec_dot_prod_ptr<const M: usize, const N: usize, R: Ring>(x: &[R], r: usize, y: &[R], out: &mut R) {
+	*out = R::zero();
+	for i in 0..N {
+		*out += x[index!(M, N, r, i)] * y[i]
+	}
+}
+
+fn mat_vec_mul_ptr<const M: usize, const N: usize, R: Ring>(a: &[R], vec: &[R], to_vec: &mut [R]) {
+	for r in 0..M {
+		vec_dot_prod_ptr::<M, N, R>(a, r, vec, &mut to_vec[r]);
+	}
+}
+
+fn mat_add<const M: usize, const N: usize, R: Ring>(a: &[R], b: &[R], out: &mut [R]) {
+	for i in 0..(M * N) {
+		out[i] = a[i] + b[i];
+	}
+}
+
+fn scalar_mul<const N: usize, R: Ring>(k: R, v: &[R], out: &mut [R]) {
+	for i in 0..N {
+		out[i] = k * v[i];
+	}
+}
+
+fn mat_mul_ptrs<const M: usize, const K: usize, const N: usize, R: Ring>(a: &[R], b: &[R], out: &mut [R]) {
+	for c in 0..N {
+		mat_vec_mul_ptr::<M, K, R>(a, 
+			&b[index!(K, N, 0, c)..index!(K, N, K, c)], 
+			&mut out[index!(M, N, 0, c)..index!(M, N, M, c)]
+		);
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+	#[test]
+	fn test_mat_vec_mul_ptr() {
+		// test the super simple identity!
+		let identity = [1.into(), 0.into(), 0.into(), 1.into()];
+		let simple_vector = [3.into(), 7.into()];
+		let mut out_vector = [0.into() ; 2];
+		mat_vec_mul_ptr::<2, 2, ZM<11>>(&identity, &simple_vector, &mut out_vector);
+
+		assert_eq!(out_vector, simple_vector);
+
+		let mat = [3.into(), 2.into(), 5.into(), 1.into(), 7.into(), 0.into()];
+		let vec = [1.into(), 4.into(), 9.into()];
+		let mut out = [0.into() ; 2];
+		mat_vec_mul_ptr::<2, 3, ZM<11>>(&mat, &vec, &mut out);
+
+		assert_eq!(out, [9.into(), 6.into()]);
+
+	}
+
+	#[test]
+	fn test_full_mat_mul() {
+		let a = [4.into(), 8.into(), 5.into(), 5.into(), 6.into(), 5.into(), 9.into(), 1.into(), 10.into(), 2.into(), 1.into(), 0.into()];
+		let b = [1.into(), 2.into(), 8.into(), 3.into(), 1.into(), 4.into(), 0.into(), 7.into()];
+
+		let mut prod = [0.into() ; 3 * 2];
+		mat_mul_ptrs::<3, 4, 2, ZM<11>>(&a, &b, &mut prod);
+		
+
+		assert_eq!(prod, [4.into(), 9.into(), 7.into(), 5.into(), 6.into(), 3.into()]);
+	}
+}
+
+#[derive(Clone, Copy)]
+struct Matrix<const M: usize, const N: usize, R: Ring> where [(); M * N]: Sized  {
+	pub flatmap: [R ; M * N]
+}
+
+impl<const M: usize, const N: usize, R: Ring> Matrix<M, N, R> where [(); M * N]: Sized  {
+
+	pub fn new() -> Self {
+		Matrix::from_flatmap([R::zero() ; M * N])
+	}
+
+	pub fn identity() -> Self {
+		let mut mat = Matrix::new();
+
+		for r in 0..M {
+			for c in 0..N {
+				if r == c {
+					mat[(r, c)] = R::one();
+				}
+			}
+		}
+
+		mat
+	}
+
+	pub fn from_flatmap(flatmap: [R ; M * N]) -> Self {
+		Matrix { flatmap }
+	}
+
+}
+
+impl<const M: usize, const N: usize, R: Ring> Index<(usize, usize)> for Matrix<M, N, R> where [(); M * N]: Sized {
+	type Output = R;
+
+	fn index(&self, index: (usize, usize)) -> &Self::Output {
+		match index {
+			(r, c) => &self.flatmap[index!(M, N, r, c)]
+		}
+	}
+}
+
+impl<const M: usize, const N: usize, R: Ring> IndexMut<(usize, usize)> for Matrix<M, N, R> where [(); M * N]: Sized {
+	fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
+		match index {
+			(r, c) => &mut self.flatmap[index!(M, N, r, c)]
+		}
+	}
+}
+
+
+impl<const M: usize, const N: usize, R: Ring> Add<Matrix<M, N, R>> for Matrix<M, N, R> where [(); M * N]: Sized {
+	type Output = Self;
+
+	fn add(self, rhs: Self) -> Self::Output {
+		let mut sum = Matrix::new();
+		mat_add::<M, N, R>(&self.flatmap, &rhs.flatmap, &mut sum.flatmap);
+		sum
+	}
+}
+
+impl<const M: usize, const N: usize, R: Ring> AddAssign<Matrix<M, N, R>> for Matrix<M, N, R> where [(); M * N]: Sized {
+	fn add_assign(&mut self, rhs: Self) {
+		for i in 0..(M * N) {
+			self.flatmap[i] += rhs.flatmap[i]
+		}
+	}
+}
+
+impl<const M: usize, const N: usize, R: Ring> Sub<Matrix<M, N, R>> for Matrix<M, N, R> where [(); M * N]: Sized {
+	type Output = Self;
+
+	fn sub(self, rhs: Self) -> Self::Output {
+		let mut sum = Matrix::new();
+
+		for i in 0..(M * N) {
+			sum.flatmap[i] = self.flatmap[i] - rhs.flatmap[i]
+		}
+
+		sum
+	}
+}
+
+impl<const M: usize, const N: usize, R: Ring> SubAssign<Matrix<M, N, R>> for Matrix<M, N, R> where [(); M * N]: Sized {
+	fn sub_assign(&mut self, rhs: Self) {
+		for i in 0..(M * N) {
+			self.flatmap[i] -= rhs.flatmap[i]
+		}
+	}
+}
+
+impl<const M: usize, const K: usize, const N: usize, R: Ring> Mul<Matrix<K, N, R>> for Matrix<M, K, R> where [(); M * K]: Sized, [(); K * N]: Sized, [(); M * N]: Sized,  {
+	type Output = Matrix<M, N, R>;
+
+	fn mul(self, rhs: Matrix<K, N, R>) -> Self::Output {
+		println!("Multiplying {:?} by {:?}", self, rhs);
+
+		let mut prod = Matrix::<M, N, R>::new();
+		mat_mul_ptrs::<M, K, N, R>(&self.flatmap, &rhs.flatmap, &mut prod.flatmap);
+		prod
+	}
+}
+
+impl<const M: usize, const N: usize, R: Ring> Neg for Matrix<M, N, R> where [(); M * N]: Sized, <R as Neg>::Output: Ring {
+	type Output = Self;
+
+	fn neg(self) -> Self::Output {
+		let mut negative = Matrix::new();
+		scalar_mul::<{M * N}, R>(-R::one(), &self.flatmap, &mut negative.flatmap);
+		negative
+	}
+}
+
+impl<const M: usize, const N: usize, R: Ring> PartialEq for Matrix<M, N, R> where [(); M * N]: Sized {
+	fn eq(&self, other: &Self) -> bool {
+		for i in 0..(M * N) {
+			if self.flatmap[i] != other.flatmap[i] {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	fn ne(&self, other: &Self) -> bool {
+		for i in 0..(M * N) {
+			if self.flatmap[i] == other.flatmap[i] {
+				return true;
+			}
+		}
+		return false;
+	}
+}
+
+impl<const M: usize, const N: usize, R: Ring> Debug for Matrix<M, N, R> where [() ; M * N]: Sized {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+		// get the widest value so we know how many spaces we need!
+		let mut widest_str_len = 0;
+		for i in 0..(M * N) {
+			let as_str = format!("{:?}", self.flatmap[i]);
+			let this_len = as_str.chars().count();
+			if this_len > widest_str_len {
+				widest_str_len = this_len;
+			}
+		}
+
+		// now, we make a vector of strings!
+		let mut lines = Vec::<String>::new();
+
+		for r in 0..M {
+			let mut this_line = Vec::<String>::new();
+
+			if r == 0 {
+				this_line.push("┌ ".to_string());
+			} else if r == M - 1 {
+				this_line.push("└ ".to_string())
+			} else {
+				this_line.push("│ ".to_string())
+			}
+
+			for c in 0..N {
+				let this_entry_str = format!("{:?}", self.flatmap[index!(M, N, r, c)]);
+				let this_entry_len = this_entry_str.chars().count();
+				this_line.push(format!("{}{}", this_entry_str, " ".repeat(widest_str_len - this_entry_len + 1)));
+			}
+
+			if r == 0 {
+				this_line.push("┐ ".to_string());
+			} else if r == M - 1 {
+				this_line.push("┘ ".to_string())
+			} else {
+				this_line.push("│ ".to_string())
+			}
+
+			lines.push(this_line.join(""));
+		}
+
+		write!(f, "\n{}", lines.join("\n"))
+	}
+}
+
+#[cfg(test)]
+mod type_tests {
+    use super::*;
+
+	#[test]
+	fn test_mat_mul() {
+		// test the super simple identity!
+		let identity = Matrix::<2, 2, ZM<11>>::identity();
+		let simple_vector = Matrix::<2, 1, ZM<11>>::from_flatmap([3.into(), 7.into()]);
+
+		let out_vector = identity * simple_vector;
+
+		assert_eq!(out_vector, simple_vector);
+
+		let mat = [3.into(), 2.into(), 5.into(), 1.into(), 7.into(), 0.into()];
+		let vec = [1.into(), 4.into(), 9.into()];
+		let mut out = [0.into() ; 2];
+		mat_vec_mul_ptr::<2, 3, ZM<11>>(&mat, &vec, &mut out);
+
+		assert_eq!(out, [9.into(), 6.into()]);
+
+	}
+
+	#[test]
+	fn test_full_mat_mul() {
+		let a = [4.into(), 8.into(), 5.into(), 5.into(), 6.into(), 5.into(), 9.into(), 1.into(), 10.into(), 2.into(), 1.into(), 0.into()];
+		let b = [1.into(), 2.into(), 8.into(), 3.into(), 1.into(), 4.into(), 0.into(), 7.into()];
+
+		let mut prod = [0.into() ; 3 * 2];
+		mat_mul_ptrs::<3, 4, 2, ZM<11>>(&a, &b, &mut prod);
+		
+
+		assert_eq!(prod, [4.into(), 9.into(), 7.into(), 5.into(), 6.into(), 3.into()]);
+	}
+}
